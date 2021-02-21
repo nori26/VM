6113d380807bdb34a77b3dff18656d6ec1153195
nori <a@a.a>
2021-02-21 23:18:25 +0100

    if

--- light.c
+++ light.c
@@ -6,7 +6,7 @@
 /*   By: nosuzuki <nosuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/20 05:36:08 by nosuzuki          #+#    #+#             */
-/*   Updated: 2021/02/21 11:28:45 by nosuzuki         ###   ########.fr       */
+/*   Updated: 2021/02/21 23:16:19 by nosuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,9 @@ int	diff_ref(t_vect point, t_vect sp, t_vect view)
 	incidence = vect_unit(vect_sub(light, point));
 	diff = dot(incidence, normal);
 	// printf("%f\n", vect_sub(vect_mult(normal, 2 * diff), incidence).len);
-	spec = pow(dot(vect_mult(view, -1 ),vect_unit(vect_sub(vect_mult(normal, 2 * diff), incidence))), GLOSS);
-	ret = (diff >= 0) * (int)(diff * 255 * DIFF) + (spec >= 0) * (int)(255 * spec * SPEC) + (int)(255 * AMB);
+	if (diff > 0)
+		spec = pow(dot(vect_mult(view, -1 ),vect_unit(vect_sub(vect_mult(normal, 2 * diff), incidence))), GLOSS);
+	ret = (diff >= 0) * (int)(diff * 255 * DIFF) + (spec >= 0) * (int)(255 * spec) + (int)(255 * AMB);
 	return (ret);
 }
 
