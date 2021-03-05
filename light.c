f602f0bbdeb68a5dc424c49ea81afdbfdf343ea2
nori <a@a.a>
2021-03-05 10:52:25 +0100

    a

--- light.c
+++ light.c
@@ -6,11 +6,13 @@
 /*   By: nosuzuki <nosuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/20 05:36:08 by nosuzuki          #+#    #+#             */
-/*   Updated: 2021/02/22 19:59:26 by nosuzuki         ###   ########.fr       */
+/*   Updated: 2021/03/05 10:52:18 by nosuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "puts.h"
+
+int i;
 
 int	light(t_vect p_point, t_vect sp, t_vect u_view)
 {
@@ -26,7 +28,11 @@ int	light(t_vect p_point, t_vect sp, t_vect u_view)
 	u_light = vect_unit(vect_sub(p_light, p_point));
 	cos_nl = dot(u_light, u_normal);
 	if (cos_nl > 0)
+	{
+		if (i++ % 5 == 1)
+			printf("v : %f\nu_l : %f\nn ; %f\ncos_nl : %f\n", u_view, u_light, u_normal, cos_nl);
 		ret += spec(u_view, u_light, u_normal, cos_nl);
+	}
 	return (ret += (cos_nl > 0) * (int)(cos_nl * 255 * DIFF));
 }
 
