8e284c7815707e7edcda8bbbda7af08dea4121e3
nori <a@a.a>
2021-03-10 11:54:12 +0100

    refact shadow

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nosuzuki <nosuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 20:55:51 by nosuzuki          #+#    #+#             */
-/*   Updated: 2021/03/10 09:25:30 by nosuzuki         ###   ########.fr       */
+/*   Updated: 2021/03/10 11:53:52 by nosuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,33 +44,37 @@ void	screen_center(t_img *img)
 
 	distance = img->w / (2 * tan(img->cam->fov / 2));
 	img->cam->cent = vect_mult(img->cam->n, distance);
-	printf("dist : %f\n", distance);
 }
 
-int i;
+double		shadow_node_dist(t_img *img, t_vect u_ray)
+{
+	void	*obj;
+	int		id;
+	t_vect	to_raystart;
+
+	id = img->lst->id;
+	obj = img->lst->obj;
+	to_raystart = img->f_ret_to_raystart[id](obj, img->ray_start);
+	return(img->f_node_judge[id](img, obj, u_ray, to_raystart));
+}
+
 void		shadow(t_img *img)
 {
-	double dist;
-	double dist_from_light;
-	t_vect u_shadow_ray;
-	t_vect ray_start;
+	double 	dist;
+	double 	dist_from_light;
+	t_vect 	u_ray;
 
-	dist= -1;
 	img->shad = 1;
 	img->light = img->l_start;
 	while (img->light)
 	{
-		u_shadow_ray = vect_unit(vect_sub(img->light->pos, img->node.pos));
-		ray_start = vect_add(img->node.pos, vect_mult(u_shadow_ray, EPSILON));
-		dist_from_light = vect_len(vect_sub(img->light->pos, ray_start));
-		img->ray_start = ray_start;
+		u_ray = vect_unit(vect_sub(img->light->pos, img->node.pos));
+		img->ray_start = vect_add(img->node.pos, vect_mult(u_ray, EPSILON));
+		dist_from_light = vect_len(vect_sub(img->light->pos, img->ray_start));
 		img->lst = img->o_start;
 		while ((img->lst))
 		{
-			dist = img->f_node_judge[img->lst->id]
-				(img, img->lst->obj, u_shadow_ray,
-				img->f_ret_to_raystart[img->lst->id](img->lst->obj, ray_start));
-			printf("dist   %f\n",dist);
+			dist = shadow_node_dist(img, u_ray);
 			if (dist != -1 && dist < dist_from_light)
 			{
 				img->light->on = OFF;
