ab9d0ae10a553f082863bdc894be213e6c8d01d9
nori <a@a.a>
2021-03-15 14:00:49 +0100

    bmp

--- bmp.c
+++ bmp.c
@@ -6,30 +6,17 @@
 /*   By: nosuzuki <nosuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 21:55:18 by nosuzuki          #+#    #+#             */
-/*   Updated: 2021/03/15 08:41:33 by nosuzuki         ###   ########.fr       */
+/*   Updated: 2021/03/15 14:00:06 by nosuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "puts.h"
 #include "mlx.h"
 
-int	bmp(t_pic *img)
+void	file_header(t_pic *img, int fd)
 {
-	int x;
-	int y;
-	int fd;
 	uint32_t size;
-	uint32_t info;
 
-	if (!img->cam)
-		exit(1);
-	if ((fd = open("miniRT.bmp",
-		O_WRONLY | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO) == -1))
-		exit(1);
-	if (img->w > BMP_MAX)
-		img->w = BMP_MAX;
-	if (img->h > BMP_MAX)
-		img->h = BMP_MAX;
 	size = img->w * img->h * 4 + 14 + 12;
 	write(fd, "BM", 2);
 	write(fd, &size, 4);
@@ -37,26 +24,65 @@ int	bmp(t_pic *img)
 	write(fd, "\0\0", 2);
 	size = 14 + 12;
 	write(fd, &size, 4);
+}
+
+void	info_header(t_pic *img, int fd)
+{
+	uint32_t info;
+
 	info = 12;
 	write(fd, &info, 4);
-	write(fd, &img->w, 4);
-	write(fd, &img->h, 4);
+	write(fd, &img->w, 2);
+	write(fd, &img->h, 2);
 	info = 1;
 	write(fd, &info, 2);
 	info = 32;
-	write(fd, &info, 4);
+	write(fd, &info, 2);
+}
+
+void	image_data(t_pic *img, int fd)
+{
+	int x;
+	int y;
+	int *buf;
+	int *tmp;
+
+	if (!(buf = malloc(img->w * img->h * 4)))
+		return ;
 	x = 0;
-	y = 0;
-	// while (x < img->h * img->w * 4)
-	// {
-	// 	write(fd, img->cam->addr + x++, 1);
-	// }
-	// write(fd, , );
-	// write(fd, , );
-	// write(fd, , );
-	// write(fd, , );
-	// write(fd, , );
-	// write(fd, , );
+	y = img->h - 1;
+	tmp = buf;
+	while (y >= 0)
+	{
+		x = 0;
+		while (x < img->w)
+		{
+			*tmp = *(int *)(img->cam->addr + (y * img->line_length + x * (img->bpp / 8)));
+			tmp++;
+			x++;
+		}
+		y--;
+	}
+	write(fd, buf, img->w * img->h * 4);
+}
+
+int		bmp(t_pic *img)
+{
+	int fd;
+
+	if (!img->cam)
+		exit(1);
+	if ((fd = open("miniRT.bmp",
+		O_WRONLY | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO)) == -1)
+		exit(1);
+	if (img->w > BMP_MAX)
+		img->w = BMP_MAX;
+	if (img->h > BMP_MAX)
+		img->h = BMP_MAX;
+
+	file_header(img, fd);
+	info_header(img, fd);
+	image_data(img, fd);
 	close(fd);
 	return (0);
 }
