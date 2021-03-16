67d52f6596786f0cbdd8a3c712cb652f8d964380
nori <a@a.a>
2021-03-16 08:49:59 +0100

    norm

--- puts.h
+++ puts.h
@@ -6,7 +6,7 @@
 /*   By: nosuzuki <nosuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 19:19:18 by nosuzuki          #+#    #+#             */
-/*   Updated: 2021/03/16 08:48:30 by nosuzuki         ###   ########.fr       */
+/*   Updated: 2021/03/16 08:49:59 by nosuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,13 @@
 # define LEFT 65361
 # define RIGHT 65363
 
-typedef struct	s_idlst
+typedef struct s_idlst
 				t_idlst;
-typedef struct	s_clist
+typedef struct s_clist
 				t_clist;
-typedef struct	s_llist
+typedef struct s_llist
 				t_llist;
-typedef struct	s_pic
+typedef struct s_pic
 				t_pic;
 enum			e_id
 {
