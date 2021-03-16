624091336d014931c9d8b5c67808e55bf5808324
nori <a@a.a>
2021-03-16 08:31:38 +0100

    norm

--- puts.h
+++ puts.h
@@ -6,7 +6,7 @@
 /*   By: nosuzuki <nosuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 19:19:18 by nosuzuki          #+#    #+#             */
-/*   Updated: 2021/03/16 08:31:18 by nosuzuki         ###   ########.fr       */
+/*   Updated: 2021/03/16 08:31:36 by nosuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,24 +101,24 @@ typedef struct	s_cy
 	double		h;
 	t_vect		to_cam;
 }				t_cy;
-typedef struct	s_tr
-{
-	t_rgb		rgb;
-	t_vect		a;
-	t_vect		b;
-	t_vect		c;
-	t_vect		ab;
-	t_vect		bc;
-	t_vect		ca;
-	t_vect		ap;
-	t_vect		bp;
-	t_vect		cp;
-	t_vect		n;
-	t_vect		cross_a;
-	t_vect		cross_b;
-	t_vect		cross_c;
-	t_vect		to_cam;
-}				t_tr;
+// typedef struct	s_tr
+// {
+// 	t_rgb		rgb;
+// 	t_vect		a;
+// 	t_vect		b;
+// 	t_vect		c;
+// 	t_vect		ab;
+// 	t_vect		bc;
+// 	t_vect		ca;
+// 	t_vect		ap;
+// 	t_vect		bp;
+// 	t_vect		cp;
+// 	t_vect		n;
+// 	t_vect		cross_a;
+// 	t_vect		cross_b;
+// 	t_vect		cross_c;
+// 	t_vect		to_cam;
+// }				t_tr;
 // typedef struct	s_node
 // {
 // 	t_rgb		rgb;
